c157760f6fd0414b5641aa026382e8a8f57f9f31
Dealga McArdle <tetha.z@gmail.com>
2012-07-01 15:35:40 +0200

    added render of frontfacing

--- svg_tests/bvp_to_svg.py
+++ svg_tests/bvp_to_svg.py
@@ -90,9 +90,60 @@ def generate_2d_draw_data(context):
     return edge_list, region
 
 
+# Candidate for refactor.
+def generate_2d_draw_list(unique_set, context):
+
+    region = context.region  
+    rv3d = context.space_data.region_3d  
+    obj = context.active_object
+    vertlist = obj.data.vertices
+
+    edge_list = []
+    for edge in unique_set:
+        local_coords = [vertlist[idx].co for idx in edge]
+        world_coords = [obj.matrix_world * point for point in local_coords]    
+        edge_as_2d = [loc3d2d(region, rv3d, point) for point in world_coords]
+        edge_list.append(edge_as_2d)
+
+    return edge_list, region
+
+
+def select_unique_edges_from(face_list, context):
+    """
+    input: face_list        contains face indices for usable faces.
+    input: context          a convenience variable.
+    
+    from the given face list, many faces will share edges, we reduce this list
+    by exluding duplicate edge geometry from the drawable list.
+    """
+    obj = context.active_object
+    edge_list_pre_unique = []
+    
+    def append_edge(idx1, idx2):
+        edge_partial = sorted((vert_list[idx1],vert_list[idx2]))
+        edge_list_pre_unique.append(tuple(edge_partial))
+  
+    # deal with quads and tris    
+    for idx, face in enumerate(face_list):
+        vert_list = obj.data.polygons[face].vertices
+
+        # last addition occurs outside the loop to avoid interupting the loop
+        # all the time.
+        for i in range(len(vert_list)-1):
+            append_edge(i, i+1)
+        append_edge(-1, 0)
+
+    unique_set = set(edge_list_pre_unique)
+    printWarning(str(len(unique_set)) + " edges!")
+    return unique_set
+    
+
+
 
 def select_front_facing(context):
     """
+    from: http://freespace.virgin.net/hugo.elias/routines/r_dot.htm
+    
     When deciding if a polygon is facing the camera, you need 
     only calculate the dot product of the normal vector of     
     that polygon, with a vector from the camera to one of the 
@@ -101,7 +152,6 @@ def select_front_facing(context):
     - If the dot product is less than zero, the polygon is facing the camera. 
     - If the value is greater than zero, it is facing away from the camera.
     """
-    construction = False
     
     region = context.region  
     rv3d = context.space_data.region_3d  
@@ -109,32 +159,29 @@ def select_front_facing(context):
     vertlist = obj.data.vertices
 
     # [ ] be in object mode
-    # [ ] unselect everything first
     
     # neat eye location code with the help of paleajed
     eye = Vector(rv3d.view_matrix[2][:3])
     eye.length = rv3d.view_distance
     eye_location = rv3d.view_location + eye  
 
-    for polygon in obj.data.polygons:
-        pnormal = obj.matrix_world * polygon.normal
+    face_list = []
+    for idx, polygon in enumerate(obj.data.polygons):
+        
         vert_index = polygon.vertices[0]
+        pnormal = obj.matrix_world * polygon.normal
         world_coordinate = obj.matrix_world * vertlist[vert_index].co
-                
-        if construction:
-            print(  "pnormal %(pnormal)s,\n"
-                    "vet_coordinate %(world_coordinate)s,\n"
-                    "cam_loc %(eye_location)s" % vars())
-            
+        
         result_vector = eye_location-world_coordinate
         dot_value = pnormal.dot(result_vector.normalized())            
+
         if dot_value < 0.0:
             polygon.select = False
         else:
             polygon.select = True
-    
+            face_list.append(idx)
      
-    return
+    return face_list
 
 
 class RenderButton(bpy.types.Operator):
@@ -145,12 +192,30 @@ class RenderButton(bpy.types.Operator):
  
     def execute(self, context):
         obname = context.active_object.name
-        print('rendering %s' % obname)
+        print('all - rendering %s' % obname)
 
-        #data = generate_2d_draw_data(context)
-        #write_svg(data)
-        select_front_facing(context)
+        data = generate_2d_draw_data(context)
+        write_svg(data)
         return{'FINISHED'}  
+
+
+
+class RenderFrontButton(bpy.types.Operator):
+    """Defines a button"""
+    bl_idname = "svg.render_front_facing"
+    bl_label = "Renders front facing to svg"
+    country = bpy.props.StringProperty()
+ 
+    def execute(self, context):
+        obname = context.active_object.name
+        print('front - rendering %s' % obname)
+
+        face_list = select_front_facing(context)
+        unique_set = select_unique_edges_from(face_list, context)
+        data = generate_2d_draw_list(unique_set, context)
+        write_svg(data)
+        return{'FINISHED'}  
+
 
 
 class SVGPanel(bpy.types.Panel):
@@ -169,11 +234,12 @@ class SVGPanel(bpy.types.Panel):
         row.label(text="Active object is: " + obj.name)
         
         # display button
-        self.layout.operator("svg.render", text='Render')
+        self.layout.operator("svg.render", text='Render All')
+        self.layout.operator("svg.render_front_facing", text='Render Front Facing')
 
 
 
-classes = [SVGPanel, RenderButton]
+classes = [SVGPanel, RenderButton, RenderFrontButton]
 
 def register():
     for i in classes:
