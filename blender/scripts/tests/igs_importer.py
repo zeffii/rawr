67c3ada435a680e239ec9468c86656e952755831
Dealga McArdle <tetha.z@gmail.com>
2012-07-17 14:48:55 +0200

    silly changes

--- blender/scripts/tests/igs_importer.py
+++ blender/scripts/tests/igs_importer.py
@@ -3,27 +3,22 @@
 filename = 'siggraphSpacecraft70.igs'
 file_in_mem = open(filename)
 
-file_structure = []
-
-counter = 0
-situa = True
-
 def makeDiv(input):
 	return input.center(80, '-')
 
+# strips newlines while appending to lines list.
+lines = []
+for line in file_in_mem:
+	lines.append(line[:-1])
 
-# section 1
-print(makeDiv('header'))
-while(situa):
-	current_line = file_in_mem.readline()
-	if current_line[0] is not ' ':
-		current_line = current_line.replace('\n','')
-		print(current_line)
-	counter += 1
 
-	if counter > 3:
-		situa = False
+for line in lines[:10]:
+	print(line)
+	
 
-# section 2
-print(makeDiv('declarations'))
-print(file_in_mem.readline())
+
+
+
+
+
+
