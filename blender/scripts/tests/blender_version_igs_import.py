76e61e652555823f8bf8da5ca332ad1e20e47771
Dealga McArdle <tetha.z@gmail.com>
2012-07-18 16:37:32 +0200

    testing assumption that blender updates local files

--- blender/scripts/tests/blender_version_igs_import.py
+++ blender/scripts/tests/blender_version_igs_import.py
@@ -127,26 +127,25 @@ def generate_paths_from_list(path_list):
             if path[0] == '126':
                 bspline_type = path[1]
                 if bspline_type == '3':
-        
                     current_path = path[19:-5]
                     cp = [float(i) for i in current_path]
                     iteration_list = list(range(0, len(cp), 3))
                     cp = [tuple(cp[i:i+3]) for i in iteration_list]
                     BSplines.append(cp)
-                    # print('3')
-                    # print(cp)
-                    # print(len(cp))
+
                 elif bspline_type == '5':
                     print('bspline v5 not handled yet')
+                    print(path)
                     continue
                     # current_path = path[22:-5]
                     # BSplines.append(current_path)
                     # print('5')
                     # print(current_path)
+
                 else:
                     print('BSpline with unhandled content: ' + bspline_type)
 
-            print('---------')
+            # print('---------')
 
     return BSplines
 
