016b72957c3148eb10fe4ec2e54dd74d3ca70116
Dealga McArdle <tetha.z@gmail.com>
2012-07-19 16:29:38 +0200

    building up personal templates to bpy dev

---
+++ blender/scripts/templates/operator_and_button_3dview.py
@@ -0,0 +1,50 @@
+import bpy
+from mathutils import Vector
+
+
+class ButtonOne(bpy.types.Operator):
+    """Defines a button"""
+    bl_idname = "svg.dostuff"
+    bl_label = "Sometype of operator"
+ 
+    def execute(self, context):
+        # do your stuff here, this will have view3d as context
+        
+        return{'FINISHED'}  
+
+
+class OperatorPanel(bpy.types.Panel):
+    """Creates a Panel in the Object properties window"""
+    bl_label = "Some Utility"
+    bl_idname = "OBJECT_PT_somefunction"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "UI"
+    bl_context = "object"
+
+    def draw(self, context):
+        layout = self.layout
+
+        obj = context.object
+        row = layout.row()
+
+        # display label and button
+        if not obj == None:
+            row.label(text="Active object is: " + obj.name)
+            self.layout.operator("svg.dostuff", text='Description of utility')
+
+
+
+classes = [OperatorPanel, ButtonOne]
+
+def register():
+    for i in classes:
+        bpy.utils.register_class(i)
+
+
+def unregister():
+    for i in classes:
+        bpy.utils.unregister_class(i)
+
+
+if __name__ == "__main__":
+    register()
